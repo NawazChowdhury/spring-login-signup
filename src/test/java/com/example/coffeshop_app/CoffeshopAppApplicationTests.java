ff93646377720f3fe700a09948534bbeb4f84907
Nawaz Chowdhury <Nawaz.Chowdhury@trios.com>
2024-06-12 12:11:54 -0400

    v1

---
+++ src/test/java/com/example/coffeshop_app/CoffeshopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeshop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeshopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
